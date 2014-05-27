a17936c3b060b92dad599c92cdcbb7579649124f
Munif Gebara Junior <munifgebara@gmail.com>
2014-05-27 10:57:13 -0300

    Inserindo e colidindo

--- main.cpp
+++ main.cpp
@@ -41,26 +41,6 @@ void mostraAluno(tipo_aluno aluno) {
     printf("%5d %-50s %4.1f\n", aluno.ra, aluno.nome, aluno.nota);
 }
 
-void inicializaHash(int tamanho) {
-    //TAREFA Verificar se o arquivo existe?
-
-
-
-    printf("Criando arquvo hash...");
-    FILE* arq = fopen("hash.arq", "wb");
-    tipo_inicio inicio;
-    inicio.cabeca.tamanho_hash = tamanho;
-    inicio.cabeca.versao = 1;
-    fwrite(&inicio, sizeof (inicio), 1, arq);
-    for (int i = 0; i < tamanho; i++) {
-        tipo_registro registro;
-        registro.proximo = -2;
-        fwrite(&registro, sizeof (registro), 1, arq);
-    }
-    fclose(arq);
-    printf("Pronto.\n");
-}
-
 int menu() {
     printf("[1] Inserir\n");
     printf("[2] Alterar\n");
@@ -72,29 +52,124 @@ int menu() {
     int opcao;
     scanf("%d", &opcao);
     return opcao;
+}
+
+
+int calculaHash(int ra){ //Função que transforma a chave (RA) em posição
+    return ra%MAX;
+}
+
+void inicializaHash(int tamanho) {
+    //TAREFA Verificar se o arquivo existe?
+    printf("Criando arquvo hash...");
+    FILE* arq = fopen("hash.arq", "wb");
+    tipo_inicio inicio;
+    inicio.cabeca.tamanho_hash = tamanho;
+    inicio.cabeca.versao = 1;
+    fwrite(&inicio, sizeof (tipo_inicio), 1, arq);
+    for (int i = 0; i < tamanho; i++) {
+        tipo_registro registro;
+        registro.proximo = -2;
+        fwrite(&registro, sizeof (tipo_registro), 1, arq);
+    }
+    fclose(arq);
+    printf("Pronto.\n");
+}
+
+FILE* abrirArquivo() {
+    FILE* arq = fopen("hash.arq", "rb+");
+    return arq;
+}
+
+void fecharArquivo(FILE* arq) {
+    //Tarefa Porque fechar o arquivo?
+    fclose(arq);
+}
+
+int calculaPosFisica(int posLogica){
+    return sizeof(tipo_inicio)+posLogica*sizeof(tipo_registro);
+}
+
+int calculaPosLogica(int posFisica){
+    return (posFisica-sizeof(tipo_inicio))/sizeof(tipo_registro);
+}
+
+
+
+void inserir(FILE* arq) {
+    //Tarefa NAO DEIXAR INSERIR RA DUPLICADO
+    tipo_registro novoRegistro,registroEmDisco;
+    recebeAluno(novoRegistro.aluno);
+    novoRegistro.proximo=-1;
+    int posLogica=calculaHash(novoRegistro.aluno.ra);
+    int posFisica=calculaPosFisica(posLogica);
+    fseek(arq,posFisica,SEEK_SET);
+    fread(&registroEmDisco,sizeof(tipo_registro),1,arq);
     
-    
+    if (registroEmDisco.proximo==-2){
+        fseek(arq,-sizeof(tipo_registro),SEEK_CUR);
+        fwrite(&novoRegistro,sizeof(tipo_registro),1,arq);
+    }
+    else{
+        printf ("A posicao %d esta ocupada.\n",posLogica);
+        fseek(arq,0,SEEK_END);
+        int posicaoLogicaNovo=calculaPosLogica(ftell(arq));
+        fwrite(&novoRegistro,sizeof(tipo_registro),1,arq);
+        fseek(arq,posFisica,SEEK_SET);
+        registroEmDisco.proximo=posicaoLogicaNovo;
+        fwrite(&registroEmDisco,sizeof(tipo_registro),1,arq);
+    }
+}
+
+void alterar(FILE* arq) {  //Tarefa Alterar NAO PODE ALTERAR A CHAVE
+
+}
+
+void excluir(FILE* arq) {  //Pensar como fazer
+
+}
+
+void procurar(FILE* arq) { //Tarefa digita o RA e diz e se existe mostra os dados
+
+}
+
+void listar(FILE* arq) {
+    fseek(arq,calculaPosFisica(0),SEEK_SET);
+    tipo_registro reg;
+    int posicao=0;
+    for (;;){
+        if (fread(&reg,sizeof(tipo_registro),1,arq)==0){
+            break;
+        }
+        printf ("Posicao %3d Proximo:%3d:",posicao++,reg.proximo);
+        if (reg.proximo!=-2){
+            mostraAluno(reg.aluno);
+        }
+        else{
+            printf ("Livre\n");
+        }
+    }
 }
 
 int main() {
-    inicializaHash(MAX);
+    //inicializaHash(MAX);
     FILE *arq = abrirArquivo();
-    int o =;
+    int o;
     do {
         o = menu();
-        switch o :
+        switch (o) {
             case 1:inserir(arq);
-        break;
-        case 2:alterar(arq);
-        break;
-        case 3:excluir(arq);
-        break;
-        case 4:procurar(arq);
-        break;
-        case 5:listar(arq);
-        break;
-        case
+                break;
+            case 2:alterar(arq);
+                break;
+            case 3:excluir(arq);
+                break;
+            case 4:procurar(arq);
+                break;
+            case 5:listar(arq);
+                break;
+        }
     } while (o != 0);
-    fecharArquivo();
+    fecharArquivo(arq);
 }
 
