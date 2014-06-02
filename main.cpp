0e175203679737b27aad601dcbda823cacf85644
Munif Gebara Junior <munifgebara@gmail.com>
2014-06-02 15:14:09 -0300

    Índice por nome

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 100
 
@@ -16,11 +17,16 @@ struct tipo_registro {
     // posição do proximo
 };
 
+struct tipo_no_arvore_nome {
+    char nome[50];
+    int posicao_no_arquivoHash;
+    int esquerda;
+    int direita;
+};
+
 struct tipo_cabeca {
     int tamanho_hash;
     int versao;
-
-
 };
 
 union tipo_inicio {
@@ -41,12 +47,19 @@ void mostraAluno(tipo_aluno aluno) {
     printf("%5d %-20s %4.1f\n", aluno.ra, aluno.nome, aluno.nota);
 }
 
+void mostraIndice(tipo_no_arvore_nome tnan) {
+    printf("%-20s %4i %4i %4i\n", tnan.nome, tnan.posicao_no_arquivoHash,
+            tnan.esquerda, tnan.direita);
+}
+
 int menu() {
     printf("[1] Inserir\n");
     printf("[2] Alterar\n");
     printf("[3] Excluir\n");
     printf("[4] Procurar\n");
     printf("[5] Listar\n");
+    printf("[6] Mostra Indice\n");
+    printf("[7] Mostra Ordenado por nome\n");
     printf("[0] Sair\n");
     printf("\nOPCAO:");
     int opcao;
@@ -58,10 +71,85 @@ int calculaHash(int ra) { //Função que transforma a chave (RA) em posição
     return ra % MAX;
 }
 
+void mostraIndicePorNome(FILE *arqIndice) {
+    fseek(arqIndice, 0, SEEK_SET);
+    int pos = 0;
+    for (;;) {
+        tipo_no_arvore_nome tnan;
+        if (0 == fread(&tnan, sizeof (tipo_no_arvore_nome), 1, arqIndice)) {
+            break;
+        }
+        printf("%03i ", pos);
+        pos++;
+        mostraIndice(tnan);
+    }
+}
+
+
+
+void insereNoIndice(FILE* arqIndi, int posRaiz, int posDados,
+        tipo_no_arvore_nome dados) {
+    tipo_no_arvore_nome tnan;
+    fseek(arqIndi, sizeof (tipo_no_arvore_nome) * posRaiz, SEEK_SET);
+    fread(&tnan, sizeof (tipo_no_arvore_nome), 1, arqIndi);
+    if (strcmp(dados.nome,tnan.nome)<0){
+        if (tnan.esquerda==-1){
+            fseek(arqIndi, -sizeof (tipo_no_arvore_nome) , SEEK_CUR);
+            tnan.esquerda=posDados;
+            fwrite(&tnan, sizeof (tipo_no_arvore_nome), 1, arqIndi);
+        }
+        else{
+            insereNoIndice(arqIndi,tnan.esquerda,posDados,dados);
+        }
+    }
+    else{
+        if (tnan.direita==-1){
+            fseek(arqIndi, -sizeof (tipo_no_arvore_nome) , SEEK_CUR);
+            tnan.direita=posDados;
+            fwrite(&tnan, sizeof (tipo_no_arvore_nome), 1, arqIndi);
+        }
+        else{
+            insereNoIndice(arqIndi,tnan.direita,posDados,dados);
+        }
+    }
+}
+
+FILE* inicializaIndicePorNome(FILE* arqHash) {
+    printf("Criando indice por nome...");
+    FILE* arqInd = fopen("indice.arq", "wb+");
+    fseek(arqHash, sizeof (tipo_inicio), SEEK_SET);
+    int pos = 0;
+    int tamIndice=0;
+    for (;;) {
+        tipo_registro reg;
+        if (0 == fread(&reg, sizeof (tipo_registro), 1, arqHash)) {
+            break;
+        }
+        if (reg.proximo != -2) {
+            tamIndice++;
+            tipo_no_arvore_nome tnan;
+            strcpy(tnan.nome, reg.aluno.nome); //tnan.nome = reg.aluno.nome;
+            tnan.posicao_no_arquivoHash = pos;
+            tnan.direita = -1;
+            tnan.esquerda = -1;
+            fwrite(&tnan, sizeof (tipo_no_arvore_nome), 1, arqInd);
+        }
+        pos++;
+    }
+    printf("inseridos...arrumando...\n");
+    for (int i = 1; i < tamIndice; i++) {
+        tipo_no_arvore_nome tnan;
+        fseek(arqInd, sizeof (tipo_no_arvore_nome) * i, SEEK_SET);
+        fread(&tnan, sizeof (tipo_no_arvore_nome), 1, arqInd);
+        insereNoIndice(arqInd, 0, i, tnan);
+    }
+    printf("pronto.\n");
+    return arqInd;
+}
+
 FILE* inicializaHash(int tamanho) {
-    //TAREFA Verificar se o arquivo existe?
     printf("Criando arquvo hash...");
-    FILE* arq = fopen("hash.arq", "wb");
+    FILE* arq = fopen("hash.arq", "wb+");
     tipo_inicio inicio;
     inicio.cabeca.tamanho_hash = tamanho;
     inicio.cabeca.versao = 1;
@@ -85,7 +173,6 @@ FILE* abrirArquivo() {
 }
 
 void fecharArquivo(FILE* arq) {
-    //Tarefa Porque fechar o arquivo?
     fclose(arq);
 }
 
@@ -97,8 +184,30 @@ int calculaPosLogica(int posFisica) {
     return (posFisica - sizeof (tipo_inicio)) / sizeof (tipo_registro);
 }
 
+
+void mostraOrdenado(FILE* arqIndi,FILE* arqDados,int pos) {
+    if (pos==-1){
+        return;
+    }
+    
+    
+    tipo_no_arvore_nome tnan;
+    fseek(arqIndi, sizeof (tipo_no_arvore_nome) * pos, SEEK_SET);
+    fread(&tnan, sizeof (tipo_no_arvore_nome), 1, arqIndi);
+    
+    mostraOrdenado(arqIndi,arqDados,tnan.esquerda);
+    tipo_registro reg;
+    fseek(arqDados, calculaPosFisica(tnan.posicao_no_arquivoHash), SEEK_SET);
+    fread(&reg, sizeof (tipo_registro), 1, arqDados);
+    mostraAluno(reg.aluno);
+    mostraOrdenado(arqIndi,arqDados,tnan.direita);
+    
+    
+}
+
+
 void inserir(FILE* arq) {
-    //Tarefa NAO DEIXAR INSERIR RA DUPLICADO
+    //Trabalho NAO DEIXAR INSERIR RA DUPLICADO
     tipo_registro novoRegistro, registroEmDisco;
     recebeAluno(novoRegistro.aluno);
     novoRegistro.proximo = -1;
@@ -123,6 +232,7 @@ void inserir(FILE* arq) {
 }
 
 //TRABALHO COMPLETAR O CÓDIGO DESTA FUNÇÃO
+
 void alterar(FILE* arq) { //Tarefa Alterar NAO PODE ALTERAR A CHAVE
     int ra;
     printf("RA    :");
@@ -146,6 +256,7 @@ void alterar(FILE* arq) { //Tarefa Alterar NAO PODE ALTERAR A CHAVE
 }
 
 //TRABALHO COMPLETAR O CÓDIGO DESTA FUNÇÃO
+
 void excluir(FILE* arq) { //Pensar como fazer
     int ra;
     printf("RA    :");
@@ -220,6 +331,7 @@ void listar(FILE * arq) {
 int main() {
     //inicializaHash(MAX);
     FILE *arq = abrirArquivo();
+    FILE *arqInd = inicializaIndicePorNome(arq);
     int o;
     do {
         o = menu();
@@ -234,8 +346,14 @@ int main() {
                 break;
             case 5:listar(arq);
                 break;
+            case 6:mostraIndicePorNome(arqInd);
+                break;
+            case 7:mostraOrdenado(arqInd,arq,0);
+                break;
+
         }
     } while (o != 0);
     fecharArquivo(arq);
+    fecharArquivo(arqInd);
 }
 
